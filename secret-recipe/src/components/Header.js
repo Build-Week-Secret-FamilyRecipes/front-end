be57730d7dbd6ab59d555e96b157b65d1c940be4
declan-casey <dpcaseyboy99@gmail.com>
2021-02-02 10:09:05 -0500

    Merging

--- secret-recipe/src/components/Header.js
+++ secret-recipe/src/components/Header.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function Header() {
+    const logout = () => {
+        localStorage.removeItem('token');
+        window.location.href =  '/';
+      }; 
     return (
         <div>
             <form className = "search-bar">
                 <input className = "bar">
                 </input>
                 <button>Search</button>
+                <button onClick = {logout}>Logout</button>
             </form>
             <Link to = "/">Home</Link>
             <Link to = "/login">Login</Link>
